fdb992fa138f4f3669d9a246537df8db487a538a
Saurabh0703 <66934981+Saurabh0703@users.noreply.github.com>
2023-08-01 12:19:55 +0530

    Update _variables.tf

--- _variables.tf
+++ _variables.tf
@@ -7,7 +7,11 @@ variable "subnet_id" {
   description = "The VPC Subnet IDs to launch in"
   type        = string
 }
-
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t4g.medium"
+}
 
 variable "project_name_prefix" {
   description = "A string value to describe prefix of all the resources"
